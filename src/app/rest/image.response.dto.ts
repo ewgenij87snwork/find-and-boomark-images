e59bc49baddf9bf348771bb2d981686a93daa149
Yevgeniy Sorokin <ewgenij87snwork@gmail.com>
2021-05-27 17:48:07 +0300

    Fixed: Unnecessary fields in ImageResponseDto

--- src/app/rest/image.response.dto.ts
+++ src/app/rest/image.response.dto.ts
@@ -1,14 +1,7 @@
 export class ImageResponseDto {
 
   id: string;
-  owner: string;
-  secret: string;
-  server: string;
-  farm: number;
   title: string;
-  ispublic: number;
-  isfriend: number;
-  isfamily: number;
   tags: string;
   // tslint:disable-next-line:variable-name
   url_q: string;
@@ -18,14 +11,7 @@ export class ImageResponseDto {
 
   constructor(data?: ImageResponseDto) {
     this.id = data.id;
-    this.owner = data.owner;
-    this.secret = data.secret;
-    this.server = data.server;
-    this.farm = data.farm;
     this.title = data.title;
-    this.ispublic = data.ispublic;
-    this.isfriend = data.isfriend;
-    this.isfamily = data.isfamily;
     this.tags = data.tags;
     this.url_q = data.url_q;
     this.photos = data.photos;
